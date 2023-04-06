4eed6e7dd835f6ac930881a946cb03bcfcccf05b
andrii <realandryxa1996@gmail.com>
2023-04-06 09:38:06 +0300

    last fix

--- src/components/NotValidUrl/NotValidUrl.styled.js
+++ src/components/NotValidUrl/NotValidUrl.styled.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 export const Text = styled.p`
   text-align: center;
-  margin-top: 800px;
+  margin-top: 200px;
   font-size: 80px;
   font-weight: 700;
   text-shadow: 2px 2px red;
